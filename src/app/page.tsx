fe456d01917ca4ac03c0e1c0dce21e738c8fcc67
darta com <awegvg@gmail.com>
2026-03-12 13:04:45 +0000

    виправ (енерги) на  ( енергия) (_for element p_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
       {/* Center Block */}
       <div className="relative w-full md:flex-1 h-[25vh] md:h-full bg-background p-4 md:p-8 flex flex-col items-center justify-center">
         <p className="mb-4 text-xs font-light text-muted-foreground text-center">
-          время=енерги<br />новая ценость
+          время=енергия<br />новая ценость
         </p>
         <CountdownTimer />
         <p className="mt-4 text-xs font-light text-muted-foreground">
