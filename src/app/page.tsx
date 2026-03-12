3f57a6e7863543d742902d9bf622d4363a6c8dad
darta com <awegvg@gmail.com>
2026-03-12 11:46:59 +0000

    це буде наша стартова сторінка , на яку користувач потрапляє коли заходи

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
+    <main className="flex h-screen w-full overflow-hidden">
+      {/* Left Block Container */}
+      <div className="w-[30%] h-full flex flex-col">
+        {/* This div creates the space at the top */}
+        <div className="flex-grow" />
+        {/* This is the gray block at the bottom */}
+        <div className="bg-secondary p-8">
+          <div className="h-48" />
+        </div>
+      </div>
+
+      {/* Center Block */}
+      <div className="flex-1 h-full bg-background p-8">
+        {/* Content will go here */}
+      </div>
+
+      {/* Right Block */}
+      <div className="w-[15%] h-full bg-black" />
+    </main>
   );
 }
