e03a615ea34f1ba497e02afa1495044d6c1d678b
darta com <awegvg@gmail.com>
2026-03-13 07:31:01 +0000

    видали цей текст (_for element h1_)

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,6 @@ export default function BlogPage() {
     <main className="flex flex-col w-full min-h-screen bg-white">
       <Navigation />
       <div className="flex-grow flex items-center justify-center">
-        <h1 className="text-4xl font-bold">Blog Page</h1>
       </div>
     </main>
   );
