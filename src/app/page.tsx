6e23816570ecf4c04867c0588110313c0d00281b
darta com <awegvg@gmail.com>
2026-03-14 17:49:18 +0000

    зроби фон сторінки яскраво білим , додай прямокутний блок чорного кольор

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { Navigation } from '@/components/navigation';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col h-screen bg-background">
+    <div className="flex flex-col h-screen bg-white">
       <Navigation />
       <main className="flex-grow flex">
         <div className="w-4/5 flex items-center justify-center">
         </div>
-        <div className="w-1/5 bg-muted/30 h-full">
+        <div className="w-1/5 bg-black h-full">
           {/* This is the right-side block. Content can be added here later. */}
         </div>
       </main>
