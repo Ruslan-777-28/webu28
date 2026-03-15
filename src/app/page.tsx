807ca0c86a7c25990fd6071aa20c36c394123b59
darta com <awegvg@gmail.com>
2026-03-15 10:33:09 +0000

    зроби тінь меншою на 20%.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,17 +42,22 @@ export default function HomePage() {
   return (
     <div className="flex flex-col md:flex-row min-h-[100vh] w-full">
       {/* Left Block */}
-      <div className="relative w-full md:flex-1 bg-white flex flex-col md:border-b-0">
+      <div className="relative w-full md:flex-1 bg-white flex flex-col">
         <div className="sticky top-0 z-30 bg-white p-4 md:p-8">
           <div className="relative z-10 flex flex-wrap items-center justify-between gap-4">
             <div className="flex items-center gap-4">
               <Link href="/" className={cn(
                 'pb-1 border-b-2 transition-colors',
                 pathname === '/'
-                    ? 'border-primary text-foreground'
-                    : 'border-transparent text-muted-foreground hover:text-foreground'
+                    ? 'border-primary'
+                    : 'border-transparent'
                 )}>
-                  <HomeIcon className='h-5 w-5' />
+                  <HomeIcon className={cn(
+                      'h-5 w-5',
+                      pathname === '/'
+                        ? 'text-foreground'
+                        : 'text-muted-foreground hover:text-foreground'
+                      )} />
               </Link>
               <span className="text-xs text-muted-foreground ml-4">простір обміну цінностями</span>
             </div>
@@ -83,7 +88,7 @@ export default function HomePage() {
 
         <div className="flex-grow flex flex-col justify-between">
            <div className="relative flex-grow flex flex-col justify-center items-center pt-[3%]" style={{minHeight: 'calc(100vh - 220px)'}}>
-              <div className="relative w-[107.2vmin] max-w-2xl h-[107.2vmin] max-h-2xl rounded-full border border-gray-200 flex items-center justify-center">
+              <div className="relative w-[112.56vmin] max-w-[calc(2rem+48rem)] h-[112.56vmin] max-h-[calc(2rem+48rem)] rounded-full border border-gray-200 flex items-center justify-center">
                   <div className="w-[80%] h-[80%] rounded-full border border-gray-200 flex items-center justify-center">
                     <div className="w-[80%] h-[80%] rounded-full border border-gray-200 flex items-center justify-center">
                         <div className="w-[80%] h-[80%] rounded-full border border-gray-200"></div>
@@ -104,14 +109,14 @@ export default function HomePage() {
       </div>
       
       {/* Center Block */}
-      <div className="w-full md:w-[27.56rem] bg-white flex justify-center p-8 md:p-0">
-         <div className="sticky top-[52%] h-fit text-gray-500">
+      <div className="w-full md:w-[28.938rem] bg-white flex justify-center p-8 md:p-0">
+         <div className="sticky h-fit text-gray-500" style={{top: 'calc(52% + 3%)'}}>
             <CountdownTimer />
          </div>
       </div>
 
       {/* Right Block */}
-      <div className="w-full md:w-1/5 bg-black relative z-10 shadow-[-12rem_0_5rem_-2.5rem_rgba(128,128,128,0.4)] flex flex-col">
+      <div className="w-full md:w-1/5 bg-black relative z-10 shadow-[-10rem_0_4rem_-2rem_rgba(128,128,128,0.32)] flex flex-col">
         <div className="sticky top-0 z-20 bg-black p-4 text-center md:pt-8">
           <h2 className="text-white font-thin text-sm tracking-widest">LECTOR</h2>
         </div>
